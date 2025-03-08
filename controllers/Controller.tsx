3410c4c11adf72960e072b777a3ad5195a1d7b23
ErFaN <safarierfan29@gmail.com>
2025-03-09 01:05:00 +0330

    upload

--- controllers/Controller.tsx
+++ controllers/Controller.tsx
@@ -1,18 +1,18 @@
 import React, { JSX } from 'react';
 import HomeLayout from '@/layouts/home/layout';
 class Controller {
-    protected modelPath=''
+    // protected modelPath=''
     protected render(data:JSX.Element):JSX.Element{
         return <HomeLayout>{data}</HomeLayout>
     }
-    protected async modelLoader(moduleName: string) {
-        const modulePaths: Record<string, string> = {
-            model: '@/models/Model',
-        };
+    // protected async modelLoader(moduleName: string) {
+    //     const modulePaths: Record<string, string> = {
+    //         model: '@/models/Model',
+    //     };
     
-        const m = await import(modulePaths[moduleName]);
-        return new m.default();
-    }
+    //     const m = await import(modulePaths[moduleName]);
+    //     return new m.default();
+    // }
     
 }
 
