3d44b5718c7ba389bab42d11a442b02f0df71c36
ErFaN <safarierfan29@gmail.com>
2025-05-30 19:49:34 +0330

    remove home layout

--- controllers/Controller.tsx
+++ controllers/Controller.tsx
@@ -1,18 +1,15 @@
 import React, { JSX } from 'react';
-import HomeLayout from '@/layouts/layout';
 import Header from '@/components/templates/header/Header';
 import Footer from '@/components/templates/footer/Footer';
 
 class Controller {
     protected render(data: JSX.Element): JSX.Element {
         return (
-            <HomeLayout>
                 <>
                     <Header/>
                     {data}
                     <Footer/>
                 </>
-            </HomeLayout>
         )
     }
 }
